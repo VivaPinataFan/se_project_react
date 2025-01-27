d73062046667bf66b8f74b7a26311a6f335862b1
VivaPinataFan <114098252+VivaPinataFan@users.noreply.github.com>
2025-01-26 19:15:28 -0700

    Header 1

---
+++ se_project_react/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css"
+import logo from"../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header() {
+    return (
+    <header className="header">
+        <img className="header__logo" src={logo} />
+        <p className="header__date-and-location">Date, Location</p>
+        <button className="header__add-clothes-btn">+ Add clothes</button>
+        <div className="header__user-container">
+            <p className="header__username">NAME</p>
+            <img src={avatar} alt="Terrence" className="user__avatar" />
+        </div>
+    </header>
+    )
+}
+
+export default Header;
