6e668a967dd8406b0d4bda7abf573b20241be23f
VivaPinataFan <114098252+VivaPinataFan@users.noreply.github.com>
2025-02-10 20:32:15 -0700

    header/weathercard progress

---
+++ se_project_react/src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import WeatherCard from "../WeatherCard/WeatherCard.jsx";
+
+function Main() {
+    return <main>
+        <WeatherCard />
+        <section className="cards">
+            <p className="cards__text">Today is 75 &deg; F / You may want to wear:</p>
+            {/*add cards*/}
+        </section>
+        
+    </main>
+}
+
+export default Main;
